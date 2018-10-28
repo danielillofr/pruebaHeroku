f939cae9484d42d6b0510e7e1762092765487057
danielillofr <dfdezruiz2@gmail.com>
2018-10-28 12:02:49 +0100

    Subida

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Escuchando puerto: ', 3000);
+});
